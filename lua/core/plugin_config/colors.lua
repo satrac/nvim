a8103f175869017ca5047822429c5e718088b16d
John Monteiro <satrac@gmail.com>
2023-04-22 12:20:28 -0400

    initial

---
+++ lua/core/plugin_config/colors.lua
@@ -0,0 +1,15 @@
+require('rose-pine').setup({ disable_background = true })
+require('gruvbox').setup()
+require('dracula').setup()
+
+function ColorMyPencils(color)
+    color = color or "gruvbox"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
+end
+
+--ColorMyPencils()
+ColorMyPencils('dracula')
+
